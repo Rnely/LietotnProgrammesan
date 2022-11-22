479ca02009f8359d21fe6669b2d3892150161803
Rnely <118372122+Rnely@users.noreply.github.com>
2022-11-22 10:45:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-import pygame, sys
-from player import Player
+import pygame 
 
-class Game:
-    def __init__(self):
-        player_sprite = Player((screen_width / 2,screen_height))
-        self.player = pygame.sprite.GroupSingle(player_sprite)
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos,constraint,speed):
+        super().__init__()
+        self.image = pygame.image.load('C:/Users/User/Desktop/Zelda-Type/app/graphics/player.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = speed
+        self.max_x_constraint = constraint
+        self.ready = True
+        self.laser_time = 0
+        self.laser_cooldown = 600
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        elif keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        
+        if keys[pygame.K_SPACE]:
+            self.shoot_laser()
+            self.ready = False
+            self.laser_time = pygame.time.get_ticks()
+
+    def recharge(self):
+        if not self.ready:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.laser_time >= self.laser_cooldown:
+                self.ready = True
+
+    def constraint(self):
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        if self.rect.right >= self.max_x_constraint:
+            self.rect.right = self.max_x_constraint
+
+    def shoot_laser(self):
+        print('shoot laser')
+
+    def update(self):
+        self.get_input()
+        self.constraint()
+        self.recharge()
 
 
-    def run(self):
-        self.player.draw(screen)
-        #update all sprite groups
-        #draw all sprite groups
-
-if __name__ == '__main__':
-    pygame.init()
-    screen_width = 600
-    screen_height = 600
-    screen = pygame.display.set_mode((screen_width,screen_height))
-    clock = pygame.time.Clock()
-    game = Game()
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
-        screen.fill((30,30,30))
-        game.run()
-
-        pygame.display.flip()
-        clock.tick(60)
