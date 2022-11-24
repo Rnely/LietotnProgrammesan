79494355beab0756409c12deb82da3cbe7d394ff
Rnely <118372122+Rnely@users.noreply.github.com>
2022-11-24 18:32:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,58 @@
-import pygame 
+import pygame, sys
+from player import Player
+import obstacle
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,constraint,speed):
-        super().__init__()
-        self.image = pygame.image.load('C:/Users/User/Desktop/Zelda-Type/app/graphics/player.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom = pos)
-        self.speed = speed
-        self.max_x_constraint = constraint
-        self.ready = True
-        self.laser_time = 0
-        self.laser_cooldown = 600
+class Game:
+    def __init__(self):
+        # player setup
+        player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
+        self.player = pygame.sprite.GroupSingle(player_sprite)
 
-    def get_input(self):
-        keys = pygame.key.get_pressed()
+        #obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 15, y_start = 480)
 
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += self.speed
-        elif keys[pygame.K_LEFT]:
-            self.rect.x -= self.speed
-        
-        if keys[pygame.K_SPACE]:
-            self.shoot_laser()
-            self.ready = False
-            self.laser_time = pygame.time.get_ticks()
+    def create_obstacle(self, x_start, y_start,offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index,col in enumerate(row):
+                if col == 'x':
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size,(241,79,80),x,y)
+                    self.blocks.add(block)
 
-    def recharge(self):
-        if not self.ready:
-            current_time = pygame.time.get_ticks()
-            if current_time - self.laser_time >= self.laser_cooldown:
-                self.ready = True
+    def create_multiple_obstacles(self,*offset,x_start,y_start):
+        for x in offset:
+            self.create_obstacle(x_start,y_start,x)
 
-    def constraint(self):
-        if self.rect.left <= 0:
-            self.rect.left = 0
-        if self.rect.right >= self.max_x_constraint:
-            self.rect.right = self.max_x_constraint
+    def run(self):
+        self.player.update()
 
-    def shoot_laser(self):
-        print('shoot laser')
+        self.player.sprite.lasers.draw(screen)
+        self.player.draw(screen)
 
-    def update(self):
-        self.get_input()
-        self.constraint()
-        self.recharge()
+        self.blocks.draw(screen)
 
+if __name__== '__main__':
+    pygame.init()
+    screen_width = 600
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width,screen_height))
+    clock = pygame.time.Clock()
+    game = Game()
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((30,30,30))
+        game.run()
+
+        pygame.display.flip()
+        clock.tick(60)
