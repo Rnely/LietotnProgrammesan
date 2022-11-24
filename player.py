8fc5c66fd6d743c6033d8a134d7a0ec01024ab8f
Rnely <118372122+Rnely@users.noreply.github.com>
2022-11-24 18:32:59 +0200

    Update player.py

--- player.py
+++ player.py
@@ -1,15 +1,18 @@
-import pygame 
+import pygame
+from laser import Laser
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,constraint,speed):
+    def __init__(self, pos, constraint, speed):
         super().__init__()
-        self.image = pygame.image.load('C:/Users/User/Desktop/Zelda-Type/app/graphics/player.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom = pos)
+        self.image = pygame.image.load('C:/Users/ttfre/PycharmProjects/pythonProject1/graphics/player.png').convert_alpha()
+        self.rect = self.image.get_rect(midbottom=pos)
         self.speed = speed
         self.max_x_constraint = constraint
         self.ready = True
         self.laser_time = 0
         self.laser_cooldown = 600
+
+        self.lasers = pygame.sprite.Group()
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -18,8 +21,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += self.speed
         elif keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
-        
-        if keys[pygame.K_SPACE]:
+
+        if keys[pygame.K_SPACE] and self.ready:
             self.shoot_laser()
             self.ready = False
             self.laser_time = pygame.time.get_ticks()
@@ -37,11 +40,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = self.max_x_constraint
 
     def shoot_laser(self):
-        print('shoot laser')
+        self.lasers.add(Laser(self.rect.center,-8,self.rect.bottom))
 
     def update(self):
         self.get_input()
         self.constraint()
         self.recharge()
-
-
+        self.lasers.update()
